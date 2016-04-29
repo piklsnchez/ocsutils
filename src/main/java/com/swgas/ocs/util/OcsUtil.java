8db904254fd6a30609046ed077d07f9e4afe3bdc
Chris DuBose <chris.dubose@swgas.com>
2016-04-29 13:05:32 -0700

    added util to ocsutil

--- src/main/java/com/swgas/ocs/util/OcsUtil.java
+++ src/main/java/com/swgas/ocs/util/OcsUtil.java
@@ -4,15 +4,71 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class OcsUtil {
-
     private static final Logger LOG = Logger.getLogger(OcsUtil.class.getName());
     public static final String ALL_ERRORS_ON_PAGE_CSS = "css=.alert-danger";
     public static final String WEBTRANID_ID           = "tranid";
     public static final String ERROR_MESSAGE_ID       = "errorMessage";
+    
+    public static String csv(String[] s){
+        StringBuilder sb = new StringBuilder();
+        for(String string : s){
+            sb.append(string).append(",");
+        }
+        int ind = sb.lastIndexOf(",");
+        return ind > -1 ? sb.toString().substring(0, ind) : sb.toString();
+    }
+    
+    public static String csvForDb(String[] s){
+        StringBuilder sb = new StringBuilder();
+        for(String string : s){
+            try{
+                Integer.parseInt(string);
+            } catch(NumberFormatException e){
+                string = "'" + string + "'";
+                if(Pattern.compile("^'\\d{4}-\\d{2}-\\d{2}'$").matcher(string).matches()){
+                    string = "DATE " + string;
+                }
+            }
+            sb.append(string).append(",");
+        }
+        return sb.toString().substring(0, sb.lastIndexOf(","));
+    }
 
+    public static String camelCase(String s){
+        if(!s.contains("_")){
+            return s;
+        }
+        boolean hump = true;
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if('_' == c){
+                hump = true;
+            } else {
+                sb.append((hump ? Character.toUpperCase(c) : Character.toLowerCase(c)));
+                hump = false;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String unCamelCase(String s){
+        if(s.contains("_")){
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            sb.append((Character.isUpperCase(c) || Character.isDigit(c) ? ("_" + c) : c));
+        }
+        if(sb.charAt(0) == '_'){
+            sb.replace(0, 1, "");
+        }
+        return sb.toString().toUpperCase();
+    }
+    
     public static <R> Optional<R> swallow(FunctionThatThrowsReturns<R> call) {
         try {
             return Optional.ofNullable(call.apply());
@@ -30,8 +86,8 @@ public class OcsUtil {
             call.apply();
         } catch (Throwable e) {
             LOG.warning(() -> String.format("swallowed: %s (Cause: %s)%n%s", e, e.getCause(), Arrays.stream(e.getStackTrace())
-                    .map((ele) -> String.format("%s:%d; ", ele.getFileName(), ele.getLineNumber()))
-                    .reduce(String::concat).orElse("?")
+                .map((ele) -> String.format("%s:%d; ", ele.getFileName(), ele.getLineNumber()))
+                .reduce(String::concat).orElse("?")
             ));
         }
     }
