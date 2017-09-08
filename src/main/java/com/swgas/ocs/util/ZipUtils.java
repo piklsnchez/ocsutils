73621ece4947ed89952c28eb805707b46ec4f95c
Chris DuBose <chris.dubose@swgas.com>
2017-09-08 15:50:48 -0700

    tweek

--- src/main/java/com/swgas/ocs/util/ZipUtils.java
+++ src/main/java/com/swgas/ocs/util/ZipUtils.java
@@ -4,23 +4,24 @@ import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class ZipUtils {
     private static final String CLASS = ZipUtils.class.getName();
     private static final Logger LOG   = Logger.getLogger(CLASS);
-    private static final Path TMP     = Paths.get(System.getProperty("java.io.tempdir"));
+    private static final int LEN = 8192;
 
     public static void unZip(InputStream in, Path outputDirectory) {
-        final int len = 8192;
+        LOG.entering(CLASS, "unZip", Stream.of(in, outputDirectory).toArray());
+        
         try(ZipInputStream zipStream = new ZipInputStream(new BufferedInputStream(in))){            
             ZipEntry entry;
             while((entry = zipStream.getNextEntry()) != null) {
                 LOG.finest(String.format("Extracting: %s", entry));
-                byte[] buff = new byte[len];
+                byte[] buff = new byte[LEN];
                 int count;
                 while((count = zipStream.read(buff)) != -1) {
                     Files.newBufferedWriter(outputDirectory.resolve(entry.getName())).append(new String(buff, 0, count)).close();
@@ -29,5 +30,6 @@ public class ZipUtils {
         } catch (Exception e) {
             LOG.severe(e.toString());
         }
+        LOG.exiting(CLASS, "unZip");
     }
 }
