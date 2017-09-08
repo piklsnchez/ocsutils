83e1a9e9fa36f0d0347f695fa05ca7a047e822e2
Chris DuBose <chris.dubose@swgas.com>
2017-09-08 16:04:25 -0700

    tweek

--- src/main/java/com/swgas/ocs/util/ZipUtils.java
+++ src/main/java/com/swgas/ocs/util/ZipUtils.java
@@ -1,6 +1,5 @@
 package com.swgas.ocs.util;
 
-import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,20 +15,23 @@ public class ZipUtils {
 
     public static void unZip(InputStream in, Path outputDirectory) {
         LOG.entering(CLASS, "unZip", Stream.of(in, outputDirectory).toArray());
-        
-        try(ZipInputStream zipStream = new ZipInputStream(new BufferedInputStream(in))){            
+        try(ZipInputStream zipStream = new ZipInputStream(in)){
+            Files.createDirectories(outputDirectory);
             ZipEntry entry;
             while((entry = zipStream.getNextEntry()) != null) {
                 LOG.finest(String.format("Extracting: %s", entry));
                 byte[] buff = new byte[LEN];
                 int count;
                 while((count = zipStream.read(buff)) != -1) {
-                    Files.newBufferedWriter(outputDirectory.resolve(entry.getName())).append(new String(buff, 0, count)).close();
+                    String name = entry.getName();
+                    LOG.info(name);
+                    Files.newBufferedWriter(outputDirectory.resolve(name)).append(new String(buff, 0, count)).close();
                 }
             }
         } catch (Exception e) {
             LOG.severe(e.toString());
         }
+        
         LOG.exiting(CLASS, "unZip");
     }
 }
